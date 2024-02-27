219c994dddd83015dee707be260cb514c71223f7
Joseph McQuade <joseph.mcquade@ovoenergy.com>
2024-02-27 13:23:01 +0000

    day6

--- src/main/scala/day6/day6.scala
+++ src/main/scala/day6/day6.scala
@@ -1,6 +1,15 @@
 package day6
 
-case class Race(time: Int, distance: Int)
+case class Race(time: Int, distance: BigInt) {
+  def run(holdTime: BigInt): BigInt =
+    holdTime * (BigInt(time) - holdTime)
+
+  def isWin(holdTime: BigInt): Boolean =
+    run(holdTime) > distance
+
+  def winningAttempts: List[Int] =
+    (0 to time).filter(isWin(_)).toList
+}
 
 val races = List(
   Race(42, 308),
@@ -10,4 +19,5 @@ val races = List(
 )
 
 @main def day6: Unit =
-  println("yarr")
+  println(races.map(_.winningAttempts.length).product)
+  println(Race(42899189, 308117012911467L).winningAttempts.length)
