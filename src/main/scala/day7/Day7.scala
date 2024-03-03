84968775cc18f229bc3f6973fea41e71a01b8ded
jwmcq <jwmcq@fingertight.co.uk>
2024-03-03 03:16:47 +0000

    yarr

--- src/main/scala/day7/Day7.scala
+++ src/main/scala/day7/Day7.scala
@@ -12,7 +12,16 @@ enum HandType:
   case FiveOfKind, FourOfKind, FullHouse, ThreeOfKind, TwoPair, OnePair,
     HighCard
 
+import HandType._
+
 case class Hand(hand: String) {
   def handType: HandType =
-    ???
+    hand.groupBy(identity).values.map(_.length).toList.sorted match
+      case List(5)          => FiveOfKind
+      case List(1, 4)       => FourOfKind
+      case List(2, 3)       => FullHouse
+      case List(1, 1, 3)    => ThreeOfKind
+      case List(1, 2, 2)    => TwoPair
+      case List(1, 1, 1, 2) => OnePair
+      case _                => HighCard
 }
