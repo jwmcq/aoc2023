2821c766c661d3a7fab0fd16123955289a92cf03
Joe McQuade <jwmcq@fingertight.co.uk>
2024-03-26 00:41:30 +0000

    day9 pt2

--- src/main/scala/day9/Day9.scala
+++ src/main/scala/day9/Day9.scala
@@ -2,16 +2,9 @@ package day9
 
 import scala.io.Source
 
-val testInput = List(
-  "0 3 6 9 12 15",
-  "1 3 6 10 15 21",
-  "10 13 16 21 30 45"
-)
-
-extension (i: Int) def |-|(j: Int): Int = (i - j).abs
-
 // question - is it possible to make Sandreadings a subtype of Seq[Int]?
 // this would make the code a lot nicer
+// maybe there's some sort of trait I should use but not sure what/how
 case class SandReadings(ints: Seq[Int]):
   def diffs: SandReadings =
     SandReadings(ints.sliding(2).map(_.reduce((a, b) => b - a)).toSeq)
@@ -27,13 +20,22 @@ case class SandReadings(ints: Seq[Int]):
           seqs.head.ints :+ (seqs.head.ints.last + loop(seqs.tail).last)
     SandReadings(loop(this.exhaustDiffs))
 
+  // legit just copy paste lol
+  def fillFirstVal: SandReadings =
+    def loop(seqs: Seq[SandReadings]): Seq[Int] =
+      seqs.isEmpty match
+        case true => Seq(0)
+        case false =>
+          (seqs.head.ints.head - loop(seqs.tail).head) +: seqs.head.ints
+    SandReadings(loop(this.exhaustDiffs))
+
 def parseInput(lines: Seq[String]): Seq[SandReadings] =
   lines.map(_.split(" ").map(_.toInt)).map(SandReadings(_))
 
 @main def day9: Unit =
-  // val input = testInput
   val input = Source.fromFile("resources/day9.txt").getLines.toSeq
   val intSeqs = parseInput(input)
   println(intSeqs.map(_.fillLastVal.ints.last).sum)
+  println(intSeqs.map(_.fillFirstVal.ints.head).sum)
 
   println("fo")
